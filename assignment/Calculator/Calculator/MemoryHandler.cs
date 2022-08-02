3068a023dbbe3cefcf24e1d54941a1f72ed092c2
Sudhanshu Nautiyal <sudhanshunautiyal777@gmail.com>
2022-08-02 11:12:10 +0530

    memory functionality added

---
+++ assignment/Calculator/Calculator/MemoryHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class MemoryHandler
+    {
+        private readonly Stack<double> _memory;
+
+        public double[] Memory { get => _memory.ToArray(); }
+        
+        public MemoryHandler()
+        {
+            _memory = new Stack<double>();
+        }
+
+        public void MemorySave(double value)
+        {
+            _memory.Push(value);
+        }
+        
+        public void MemoryClear()
+        {
+            _memory.Clear();
+        }
+        
+        public double MemoryRecall()
+        {
+            return _memory.Peek();
+        }
+        
+        public void MemoryModification(double value)
+        {
+            if(_memory.Count == 0)
+            {
+                _memory.Push(value);
+            }
+            else
+            {
+                _memory.Push(_memory.Pop() + value);
+            }
+        }
+    }
+}
