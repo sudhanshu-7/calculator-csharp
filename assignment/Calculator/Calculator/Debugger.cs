ad5fe6ebbe201579d271bb50ccc367b5ef888a2e
Sudhanshu Nautiyal <sudhanshunautiyal777@gmail.com>
2022-08-03 11:42:05 +0530

    added  a debugger class for debugging

---
+++ assignment/Calculator/Calculator/Debugger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Calculator
+{
+    internal static class Debugger
+    {
+        public static void debug(List<string> array)
+        {
+            foreach(string obj in array)
+            {
+                Console.Write(obj + " -> ");
+            }
+            Console.WriteLine(" X ");
+        }
+    }
+}
